4a86d7f06f854354337118cce44568bcca67f180
joao <joaovitorlimadearaujo@gmail.com>
2026-04-30 12:23:03 -0300

    initial commit

--- Mao.py
+++ Mao.py
@@ -1,3 +1,14 @@
+from abc import ABC, abstractmethod
+
 class Mao:
     def __init__(self):
         self.__minhasCartas = []
+
+
+    @abstractmethod
+    def extraiValor(self, outra):
+        pass
+
+    def showValue(self, value):
+        values_to_combinations = { 
+        }
